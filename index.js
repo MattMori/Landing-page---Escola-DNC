54a4ebdac9cf50f27f2de28ad4f13cf2d8bcb05c
MattMori <Mateus5310@live.com>
2023-06-20 23:35:27 -0300

    primeiro Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var leonardo = window.document.getElementById("Leonardo")
+var samantha = window.document.getElementById("Samantha")
+var bruna = window.document.getElementById("Bruna")
+
+function rolarParaDireita(){
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex; margin-top:55px"
+    
+}
+function rolarParaEsquerda(){
+    leonardo.style ="display:flex"
+    bruna.style ="display:none"
+    setaEsquerda.style = "display:none"
+    setaDireita.style = "display:flex margin-top:55px"
+}
